a7b946c067dcbbedfb66a9799023d1a0db895ac9
Yosua Onesimus <arrow_405@yahoo.com>
2013-03-30 21:38:25 +0700

    fixing some function.

---
+++ sample-projects/simple-inventory/src/view/form/PnlInventory3.java
@@ -0,0 +1,135 @@
+package view.form;
+
+public class PnlInventory extends javax.swing.JPanel {
+
+    public PnlInventory() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnlEditor = new javax.swing.JPanel();
+        btnEdit = new cococare.swing.component.CCButton();
+        btnCancel = new cococare.swing.component.CCButton();
+        btnSave = new cococare.swing.component.CCButton();
+        btnClose = new cococare.swing.component.CCButton();
+        xCode = new javax.swing.JLabel();
+        txtCode = new javax.swing.JTextField();
+        xName = new javax.swing.JLabel();
+        txtName = new javax.swing.JTextField();
+        xDescription = new javax.swing.JLabel();
+        scrDescription = new javax.swing.JScrollPane();
+        txtDescription = new javax.swing.JTextArea();
+
+        setMinimumSize(new java.awt.Dimension(600, 400));
+        setPreferredSize(new java.awt.Dimension(600, 400));
+
+        pnlEditor.setMinimumSize(new java.awt.Dimension(600, 400));
+        pnlEditor.setPreferredSize(new java.awt.Dimension(600, 400));
+
+        btnEdit.setText("Edit");
+        btnEdit.setType(cococare.swing.component.CCButton.Type.EDIT);
+
+        btnCancel.setText("Cancel");
+        btnCancel.setType(cococare.swing.component.CCButton.Type.CANCEL);
+
+        btnSave.setText("Save");
+        btnSave.setType(cococare.swing.component.CCButton.Type.SAVE);
+
+        btnClose.setText("Close");
+        btnClose.setType(cococare.swing.component.CCButton.Type.CLOSE);
+
+        xCode.setFont(new java.awt.Font("Verdana", 1, 12)); // NOI18N
+        xCode.setText("Code");
+
+        txtCode.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+
+        xName.setFont(new java.awt.Font("Verdana", 1, 12)); // NOI18N
+        xName.setText("Name");
+
+        txtName.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+
+        xDescription.setFont(new java.awt.Font("Verdana", 1, 12)); // NOI18N
+        xDescription.setText("Description");
+
+        txtDescription.setColumns(20);
+        txtDescription.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        txtDescription.setRows(5);
+        scrDescription.setViewportView(txtDescription);
+
+        javax.swing.GroupLayout pnlEditorLayout = new javax.swing.GroupLayout(pnlEditor);
+        pnlEditor.setLayout(pnlEditorLayout);
+        pnlEditorLayout.setHorizontalGroup(
+            pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlEditorLayout.createSequentialGroup()
+                .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(pnlEditorLayout.createSequentialGroup()
+                .addComponent(xCode, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(txtCode, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(pnlEditorLayout.createSequentialGroup()
+                .addComponent(xName, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(pnlEditorLayout.createSequentialGroup()
+                .addComponent(xDescription, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(scrDescription, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        pnlEditorLayout.setVerticalGroup(
+            pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlEditorLayout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addGroup(pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(11, 11, 11)
+                .addGroup(pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(xCode, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtCode, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(1, 1, 1)
+                .addGroup(pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(xName, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(1, 1, 1)
+                .addGroup(pnlEditorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(xDescription, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(scrDescription, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlEditor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlEditor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private cococare.swing.component.CCButton btnCancel;
+    private cococare.swing.component.CCButton btnClose;
+    private cococare.swing.component.CCButton btnEdit;
+    private cococare.swing.component.CCButton btnSave;
+    private javax.swing.JPanel pnlEditor;
+    private javax.swing.JScrollPane scrDescription;
+    private javax.swing.JTextField txtCode;
+    private javax.swing.JTextArea txtDescription;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JLabel xCode;
+    private javax.swing.JLabel xDescription;
+    private javax.swing.JLabel xName;
+    // End of variables declaration//GEN-END:variables
+}
