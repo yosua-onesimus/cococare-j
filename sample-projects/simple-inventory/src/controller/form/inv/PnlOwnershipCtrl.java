9954a29be3d819971c580464523723484021b987
Yosua Onesimus <arrow_405@yahoo.com>
2013-04-01 15:34:39 +0700

    fixing some function, and update recruitment.txt.

---
+++ sample-projects/simple-inventory/src/controller/form/inv/PnlOwnershipCtrl.java
@@ -0,0 +1,19 @@
+package controller.form;
+
+//<editor-fold defaultstate="collapsed" desc=" import ">
+import cococare.framework.swing.CFSwingCtrl;
+import model.obj.InvEmployee;
+//</editor-fold>
+
+public class PnlEmployeeCtrl extends CFSwingCtrl {
+
+    @Override
+    protected Class _getEntity() {
+        return InvEmployee.class;
+    }
+
+    @Override
+    protected BaseFunction _getBaseFunction() {
+        return BaseFunction.FORM_FUNCTION;
+    }
+}
