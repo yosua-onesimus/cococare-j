8cf6481b5c828dbdf7e9b709972e1b7af88fed92
Yosua Onesimus <arrow_405@yahoo.com>
2013-04-04 12:04:24 +0700

    add simple-notes.

---
+++ sample-projects/simple-notes/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+//<editor-fold defaultstate="collapsed" desc=" import ">
+import cococare.framework.model.mdl.note.NotesMain;
+//</editor-fold>
+
+public class Main {
+
+    public static void main(String[] args) {
+        //cococare-j has application notes that can be used for notes about the making of the application.
+        new NotesMain().showScreen();
+    }
+}
---
+++ sample-projects/simple-notes/src/main/test.java
@@ -0,0 +1,64 @@
+package main;
+
+import cococare.common.CCFormat;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class test {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Yosua";
+        Schedule schedule = new Schedule();
+        schedule.person = person;
+        schedule.dateBegin = CCFormat.getDate("201304011200", "yyyyMMddHHmm");
+        schedule.dateEnd = CCFormat.getNextHour(schedule.dateBegin);
+        List<Schedule> schedules = new ArrayList();
+        schedules.add(schedule);
+
+        Schedule schedule2 = new Schedule();
+        schedule2.person = person;
+        schedule2.dateBegin = CCFormat.getDate("201304081000", "yyyyMMddHHmm");
+        schedule2.dateEnd = CCFormat.getNextHour(schedule2.dateBegin);
+        schedules.add(schedule2);
+
+        Schedule schedule3 = new Schedule();
+        schedule3.person = person;
+        schedule3.dateBegin = CCFormat.getDate("201304021130", "yyyyMMddHHmm");
+        schedule3.dateEnd = CCFormat.getNextMinute(schedule3.dateBegin, 30);
+
+        for (Schedule s : schedules) {
+            int i;
+            System.out.println(s.dateBegin);
+            System.out.println(i = CCFormat.getDayOfWeek(s.dateBegin));
+            System.out.println(CCFormat.getDayName(i));
+            if (!schedule3.dateEnd.after(s.dateBegin) || !schedule3.dateBegin.before(s.dateEnd)) {
+            } else {
+                System.out.println("fail");
+            }
+        }
+    }
+}
+
+enum Repetition {
+
+    ONCE, WEEKLY, RANGE;
+}
+
+class Person {
+
+    String name;
+}
+
+class Schedule {
+
+    Repetition repetition;
+    Date dateBegin;
+    Date dateEnd;
+    Time timeBegin;
+    Time timeEnd;
+    String description;
+    Person person;
+}
